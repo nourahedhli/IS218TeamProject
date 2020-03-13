7e6e114dd473c70dbd0080863cab9efa27ffd878
Noura.Hedhli <nh98@njit.edu>
2020-03-13 16:09:58 -0400

    Helper Functions

---
+++ HelperFunctions/RandomNumberGenerator.py
@@ -0,0 +1,3 @@
+def RandomNumber():
+    rand_value = random.randint(1, 50)
+    return rand_value
---
+++ HelperFunctions/TypeTesting.py
@@ -0,0 +1,8 @@
+
+
+def typeFunction(a):
+    for i in data:
+        if type(i) == str:
+            return False
+        else:
+            return True
