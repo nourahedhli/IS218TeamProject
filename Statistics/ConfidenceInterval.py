23ab0bbfad7f2443d94c2a4ac4b677e4bfbd0efc
afridakarim <ak2577@njit.edu>
2020-03-13 15:38:09 -0400

    Add: ConfidenceInterval

---
+++ Statistics/ConfidenceInterval.py
@@ -0,0 +1,20 @@
+from statistics import mean
+from Statistics.standardDeviation import StandardDeviationSample
+from Statistics.Zscores import Z_scores
+
+
+from Statistics.Zscores import z_values
+
+
+def mean_confidence_interval(data):
+    n = len(data)
+    List = []
+    std = StandardDeviationSample(data)
+    mu = mean(data)
+    z_list = Z_scores(z_values(data))
+    SE = std / n
+    for i in z_list:
+        left = mu - i * SE
+        right = mu + i * SE
+        List.append([left, right])
+    return List
