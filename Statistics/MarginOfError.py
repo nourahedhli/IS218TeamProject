4905179cb29b84d05adf519b1c8435cc0741ad0f
afridakarim <ak2577@njit.edu>
2020-03-13 15:37:04 -0400

    Add: margin of error

---
+++ Statistics/MarginOfError.py
@@ -0,0 +1,13 @@
+from Statistics.Zscores import *
+from Statistics.standardDeviation import StandardDeviationSample
+from Calculator.squareRoot import squareRoot
+
+
+def MarginError(data):
+    List = []
+    SE = (StandardDeviationSample(data) / (squareRoot(len(data))))
+    for i in Z_scores(z_values(data)):
+        ME = i * SE
+        List.append(ME)
+
+    return Lis
